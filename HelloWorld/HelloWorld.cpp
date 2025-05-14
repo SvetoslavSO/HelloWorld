46f865d98cd7ee800952bd99cbfda244e8552efe
b3tlejuice <ascyur@yandex.ru>
2025-05-14 14:09:29 +0300

    added b3tlesquare() for debug

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+int b3tlesquare(int x) {
+    return x * x;
+}
+
 int main()
 {
     cout << endl;
@@ -11,6 +15,6 @@ int main()
     cout << "Kogda je Misha apnet 10lvl FaceIt" << endl;
     cout << "nemkevichgeorg's commit to Svetik project" << endl;
     cout << "new nemkevichgeorg's extra line" << endl;
-    cout << "b3tlejuice's debug commit" << endl; //also fixed all spaghetti code
+    cout << "b3tlejuice's debug commit for the square of 4 ->" << b3tlesquare(4) << endl;
 
 }
