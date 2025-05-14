3c577135e678e921664cde73f1c04870bc2e6735
b3tlejuice <ascyur@yandex.ru>
2025-05-14 14:25:17 +0300

    Added two comments

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -9,8 +9,9 @@ int main()
     cout << "Misha's commit to Svetoslav project" << endl;
     cout << "extra line" << endl;
     cout << "Kogda je Misha apnet 10lvl FaceIt" << endl;
-    cout << "nemkevichgeorg's commit to Svetik project" << endl;
+    cout << "nemkevichgeorg's commit to Svetik project" << endl; //1234
     cout << "new nemkevichgeorg's extra line" << endl;
     cout << "b3tlejuice's debug commit" << endl; //also fixed all spaghetti code
+    //new commit debug
 
 }
